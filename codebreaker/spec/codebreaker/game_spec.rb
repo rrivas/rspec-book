e453fb74709e43a7895dd9719fc2b78003a6af25
Robert Rivas <robertrivas@gmail.com>
2012-06-25 21:26:33 -0700

    Added a test for 2 number matches

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -35,7 +35,7 @@ module Codebreaker
         end
       end
 
-      context 'width 1 exact match' do
+      context 'with 1 exact match' do
         it "sends a mark with '+'" do
           game.start('1234')
           output.should_receive(:puts).with('+')
@@ -43,6 +43,14 @@ module Codebreaker
         end
       end
 
+      context 'with 2 number matches' do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+      
     end
   end
 end
