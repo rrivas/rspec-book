c029b84186a122144c720706a4bbee2d0acb4930
Robert Rivas <robertrivas@gmail.com>
2012-06-25 20:36:37 -0700

    Refactored to have one output for guess methode.  Added guess method

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -3,9 +3,20 @@ module Codebreaker
     def initialize(output)
       @output = output
     end
-    def start(secret = nil)
+    def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
+    end
+    def guess(guess)
+      if @secret.include?(guess[0])
+        mark = '-'
+
+      else
+        mark = ''
+      end
+
+      @output.puts mark
     end
   end
 end
