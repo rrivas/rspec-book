b592b59fece0f9cd60700ae15a09c4cb9fc08aed
Robert Rivas <robertrivas@gmail.com>
2012-06-25 20:57:32 -0700

    return a '+' if the first letter/number of a guess matches the secret

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -3,19 +3,22 @@ module Codebreaker
     def initialize(output)
       @output = output
     end
+
     def start(secret)
       @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
-    def guess(guess)
-      if @secret.include?(guess[0])
-        mark = '-'
 
+    def guess(guess)
+      if guess[0] == @secret[0]
+        mark = '+'
+      elsif @secret.include?(guess[0])
+        mark = '-'
       else
         mark = ''
       end
-
+      
       @output.puts mark
     end
   end
