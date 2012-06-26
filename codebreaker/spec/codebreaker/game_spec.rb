b83dcd985ee720ebf6078394e40e67f91248b9c5
Robert Rivas <robertrivas@gmail.com>
2012-06-25 20:57:53 -0700

    Added a test for 1 exact match

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -26,15 +26,23 @@ module Codebreaker
           game.guess('5555')
         end
       end
-    end
 
-    context 'with 1 number match' do
-      it "sends a mark with '-'" do
-        game.start('1234')
-        output.should_receive(:puts).with('-')
-        game.guess('2555')
+      context 'with 1 number match' do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
       end
-    end
 
+      context 'width 1 exact match' do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+    end
   end
 end
