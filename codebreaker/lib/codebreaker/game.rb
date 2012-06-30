786729a9dfb8e82293de7b799fd1cf2d46489e5f
Robert Rivas <robertrivas@gmail.com>
2012-06-29 18:53:43 -0700

    Added an exact match count temp variable

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,11 +11,12 @@ module Codebreaker
     end
 
     def guess(guess)
+      exact_match_count = 0
       mark = ''
 
       (0..3).each do |index|
         if exact_match?(guess, index)
-          mark << '+'
+          exact_match_count += 1
         end
       end
       
@@ -25,7 +26,7 @@ module Codebreaker
         end
       end
 
-      @output.puts mark
+      @output.puts '+'*exact_match_count + mark
     end
 
     def exact_match?(guess, index)
