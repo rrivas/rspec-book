77c57cce039a941ccc0639f3967d15864d727703
Robert Rivas <robertrivas@gmail.com>
2012-06-29 23:04:25 -0700

    Added a maker file and moved all marker class code to it.  Added require for marker class in lib/codbreaker.rb

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -15,31 +15,5 @@ module Codebreaker
 
       @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
     end
-
-    class Marker
-      def initialize(secret, guess)
-        @secret, @guess = secret, guess
-      end
-
-      def exact_match_count
-        (0..3).inject(0) do |count, index|
-          count + ( exact_match?(@guess, index) ? 1 : 0 )
-        end
-      end
-
-      def number_match_count
-        (0..3).inject(0) do |count, index|
-          count + ( number_match?(@guess, index) ? 1 : 0 )
-        end
-      end
-
-      def exact_match?(guess, index)
-        @guess[index] == @secret[index]
-      end
-
-      def number_match?(guess, index)
-        @secret.include?(@guess[index]) && !exact_match?(guess, index)
-      end
-    end
   end
 end
