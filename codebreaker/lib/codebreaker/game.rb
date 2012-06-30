d15bac08b30cfa36a0dfa1c3b7a32c14e404825e
Robert Rivas <robertrivas@gmail.com>
2012-06-29 22:09:48 -0700

    Refactored number match count message to use inject and return value rather than temp variables

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -15,24 +15,15 @@ module Codebreaker
     end
 
     def exact_match_count(guess)
-      exact_match_count = 0
-
       (0..3).inject(0) do |count, index|
-        count + (exact_match?(guess, index) ? 1 : 0 )
+        count + ( exact_match?(guess, index) ? 1 : 0 )
       end
     end
 
     def number_match_count(guess)
-      number_match_count = 0
-
-      
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
+      (0..3).inject(0) do |count, index|
+        count + ( number_match?(guess, index) ? 1 : 0 )
       end
-
-      number_match_count
     end
 
     def exact_match?(guess, index)
