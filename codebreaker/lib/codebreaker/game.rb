89cb42a7220cdf1e17f56a2268963e15e332776d
Robert Rivas <robertrivas@gmail.com>
2012-06-29 22:46:02 -0700

    Added marker instance variables to eliminate most param passing

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,34 +11,34 @@ module Codebreaker
     end
 
     def guess(guess)
-      Marker.new(@secret)
+      marker = Marker.new(@secret, guess)
 
-      @output.puts '+'*marker.exact_match_count(guess) + '-'*marker.number_match_count(guess)
+      @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
     end
 
     class Marker
-      def initialize(secret)
-        @secret = secret
+      def initialize(secret, guess)
+        @secret, @guess = secret, guess
       end
 
-      def exact_match_count(guess)
+      def exact_match_count
         (0..3).inject(0) do |count, index|
-          count + ( exact_match?(guess, index) ? 1 : 0 )
+          count + ( exact_match?(@guess, index) ? 1 : 0 )
         end
       end
 
-      def number_match_count(guess)
+      def number_match_count
         (0..3).inject(0) do |count, index|
-          count + ( number_match?(guess, index) ? 1 : 0 )
+          count + ( number_match?(@guess, index) ? 1 : 0 )
         end
       end
 
       def exact_match?(guess, index)
-        guess[index] == @secret[index]
+        @guess[index] == @secret[index]
       end
 
       def number_match?(guess, index)
-        @secret.include?(guess[index]) && !exact_match?(guess, index)
+        @secret.include?(@guess[index]) && !exact_match?(guess, index)
       end
     end
   end
